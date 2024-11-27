dc53257a598c6f963f0372e95a9501c1845af0ca
Gustavo Fukushima <gustavo.fukushima@sc.senai.br>
2024-11-27 08:57:53 -0300

    obtém registros a partir de 1999

--- nfcrawler/nfcrawler/spiders/compras_prefeitura_spider.py
+++ nfcrawler/nfcrawler/spiders/compras_prefeitura_spider.py
@@ -1,3 +1,5 @@
+import datetime
+
 import scrapy
 
 
@@ -5,38 +7,49 @@ class ContratosPrefeituraSpider(scrapy.Spider):
     name = 'contratos-prefeitura'
 
     url = 'https://transparencia.e-publica.net:443/epublica-portal/rest/florianopolis/api/v1/contrato'
-    periodo_inicial = '20/10/2024'
-    periodo_final = '20/11/2024'
+    ano_inicial = 1999
+    ano_final = datetime.date.today().year
+    data_atual = datetime.date.today()
     codigo_unidade = None
     # codigo_unidade = 1
 
     def start_requests(self):
-        self.url = self.url + f'?periodo_inicial={self.periodo_inicial}&periodo_final={self.periodo_final}'
-        if self.codigo_unidade:
-            self.url += f'&codigo_unidade={self.codigo_unidade}'
         yield scrapy.Request(url=self.url, callback=self.parse)
+        for ano in range(self.ano_inicial, self.ano_final + 1):
+            if ano == self.ano_final:
+                url = self.url + f'?periodo_inicial=01/01/{ano}&periodo_final={self.data_atual.day}/{self.data_atual.month}/{self.data_atual.year}'
+            else:
+                url = self.url + f'?periodo_inicial=01/01/{ano}&periodo_final=31/12/{ano}'
+            if self.codigo_unidade:
+                url += f'&codigo_unidade={self.codigo_unidade}'
+            res = scrapy.Request(url=url, callback=self.parse)
+            yield res
 
     def parse(self, response):
         resp_rows = response.json()
         for i in range(len(resp_rows['registros'])):
             yield resp_rows['registros'][i]
-        # proxima_pagina = response.xpath('/html/body/div[1]/div/portal-shell/section/div/div[1]/div/div/div/div[3]/div[2]/a[2]').get()
-        # if proxima_pagina:
-        #     yield response.follow(url=proxima_pagina, callback=self.parse)
 
 class LicitacoesPrefeituraSpider(scrapy.Spider):
     name = 'licitacoes-prefeitura'
 
     url = 'https://transparencia.e-publica.net:443/epublica-portal/rest/florianopolis/api/v1/licitacao'
-    periodo_inicial = '20/10/2024'
-    periodo_final = '20/11/2024'
-    codigo_unidade = 1
+    ano_inicial = 1999
+    ano_final = datetime.date.today().year
+    codigo_unidade = None
+    # codigo_unidade = 1
 
     def start_requests(self):
-        self.url = self.url + f'?periodo_inicial={self.periodo_inicial}&periodo_final={self.periodo_final}'
-        if self.codigo_unidade:
-            self.url += f'&codigo_unidade={self.codigo_unidade}'
         yield scrapy.Request(url=self.url, callback=self.parse)
+        for ano in range(self.ano_inicial, self.ano_final + 1):
+            if ano == self.ano_final:
+                url = self.url + f'?periodo_inicial=01/01/{ano}&periodo_final={self.data_atual.day}/{self.data_atual.month}/{self.data_atual.year}'
+            else:
+                url = self.url + f'?periodo_inicial=01/01/{ano}&periodo_final=31/12/{ano}'
+            if self.codigo_unidade:
+                url += f'&codigo_unidade={self.codigo_unidade}'
+            res = scrapy.Request(url=url, callback=self.parse)
+            yield res
 
     def parse(self, response):
         resp_rows = response.json()
